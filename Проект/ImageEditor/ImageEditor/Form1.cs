fe46d2d200dc0c48fbfccb5a2e42fe3f465f84c8
Alexandr <ovdienkoalexandr@mail.ru>
2017-05-04 15:41:41 +0300

    Проект
    Почти

---
+++ Проект/ImageEditor/ImageEditor/Form1.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using ImageEditor;
+using System.IO;
+
+namespace ImageEditor
+{
+    public partial class Form1 : Form
+    {
+        private Bitmap bitmap;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void dateTimePicker1_ValueChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog file_dialog = new OpenFileDialog();
+            file_dialog.Filter = "All files|*.*|PNG|*.png|JPEGs|*.jpg|Bitmaps|*.bmp|GIFs|*.gif";
+            file_dialog.FilterIndex = 1;
+            if (file_dialog.ShowDialog() == DialogResult.OK)
+            {
+                StreamReader streamReader = new StreamReader(file_dialog.FileName);
+                bitmap = (Bitmap)Bitmap.FromStream(streamReader.BaseStream);
+                streamReader.Close();
+
+                //bitmap = bitmap.CopyToSquareCanvas(StartPicture.Width);
+                StartPicture.Image = bitmap;
+
+                ChooseFileBtn.Text = file_dialog.FileName;
+            }
+        }
+
+        private void SobelBtn_Click(object sender, EventArgs e)
+        {
+            ResultPicture.Image =  bitmap.Sobel3x3Filter(GaussianCB.Checked, BlackWhiteCB.Checked);
+        }
+
+
+        private void Sobel5x5Btn_Click(object sender, EventArgs e)
+        {
+            ResultPicture.Image = bitmap.Sobel5x5Filter(GaussianCB.Checked, BlackWhiteCB.Checked);
+        }
+
+        private void Kirsch_Click(object sender, EventArgs e)
+        {
+            ResultPicture.Image = bitmap.KirschFilter(GaussianCB.Checked, BlackWhiteCB.Checked);
+        }
+
+        private void Prewitt_Click(object sender, EventArgs e)
+        {
+            ResultPicture.Image = bitmap.PrewittFilter(GaussianCB.Checked, BlackWhiteCB.Checked);
+        }
+
+        private void Laplacian3x3Btn_Click(object sender, EventArgs e)
+        {
+            ResultPicture.Image = bitmap.Laplacian3x3Filter(GaussianCB.Checked, BlackWhiteCB.Checked);
+        }
+
+        private void Laplacian5x5Btn_Click(object sender, EventArgs e)
+        {
+            ResultPicture.Image = bitmap.Laplacian5x5Filter(GaussianCB.Checked, BlackWhiteCB.Checked);
+        }
+    }
+}
---
+++ Проект/ImageEditor/ImageEditor/Matrixs.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ImageEditor
+{
+    public static class Matrix
+    {
+        public static double[,] Laplacian3x3
+        {
+            get
+            {
+                return new double[,]
+                { { -1, -1, -1,  },
+                  { -1,  8, -1,  },
+                  { -1, -1, -1,  }, };
+            }
+        }
+
+        public static double[,] Laplacian5x5
+        {
+            get
+            {
+                return new double[,]
+                { { -1, -1, -1, -1, -1, },
+                  { -1, -1, -1, -1, -1, },
+                  { -1, -1, 24, -1, -1, },
+                  { -1, -1, -1, -1, -1, },
+                  { -1, -1, -1, -1, -1  }, };
+            }
+        }
+
+
+        public static double[,] Gaussian3x3
+        {
+            get
+            {
+                return new double[,]
+                { { 1, 2, 1, },
+                  { 2, 4, 2, },
+                  { 1, 2, 1, }, };
+            }
+        }
+
+
+        public static double[,] Sobel3x3Horizontal
+        {
+            get
+            {
+                return new double[,]
+                { { -1,  0,  1, },
+                  { -2,  0,  2, },
+                  { -1,  0,  1, }, };
+            }
+        }
+
+        public static double[,] Sobel3x3Vertical
+        {
+            get
+            {
+                return new double[,]
+                { {  1,  2,  1, },
+                  {  0,  0,  0, },
+                  { -1, -2, -1, }, };
+            }
+        }
+        public static double[,] Sobel5x5Horizontal
+        {
+            get
+            {
+                return new double[,]
+                { { -1, -2, 0, 2,1 },
+                  { -4, -8, 0, 8,4 },
+                  { -6, -12, 0, 12,6 },
+                  { -4, -8, 0, 8,4 },
+                  { -1, -2, 0, 2,1 }, };
+            }
+        }
+        public static double[,] Sobel5х5Vertical
+        {
+            get
+            {
+                return new double[,]
+                { { 1, 4, 6, 4, 1 },
+                  { 2, 8, 12, 8, 2 },
+                  { 0, 0, 0, 0, 0 },
+                  { -2,-8,-12,-8,-2},
+                  { -1,-4,-6,-4,-1}, };
+            }
+        }
+        public static double[,] Prewitt3x3Horizontal
+        {
+            get
+            {
+                return new double[,]
+                { { -1,  0,  1, },
+                  { -1,  0,  1, },
+                  { -1,  0,  1, }, };
+            }
+        }
+
+        public static double[,] Prewitt3x3Vertical
+        {
+            get
+            {
+                return new double[,]
+                { {  1,  1,  1, },
+                  {  0,  0,  0, },
+                  { -1, -1, -1, }, };
+            }
+        }
+
+
+        public static double[,] Kirsch3x3Horizontal
+        {
+            get
+            {
+                return new double[,]
+                { {  5,  5,  5, },
+                  { -3,  0, -3, },
+                  { -3, -3, -3, }, };
+            }
+        }
+
+        public static double[,] Kirsch3x3Vertical
+        {
+            get
+            {
+                return new double[,]
+                { {  5, -3, -3, },
+                  {  5,  0, -3, },
+                  {  5, -3, -3, }, };
+            }
+        }
+    }
+}
